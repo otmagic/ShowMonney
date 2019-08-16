2bdf1bd50ae78c07d99c9c822f8aeaa14565a653
otmagic <ot.shen@otmagic.com>
2019-08-16 22:26:18 +0800

    no message

--- xmlParsing.py
+++ xmlParsing.py
@@ -9,29 +9,49 @@ import json
 import xmltodict
 import pymongo
 
+# 时间处理
+import time
+
+# 本地时间
+localTime = time.strftime("%Y%m%d%H%M%S")
+
 # 定义xml转json的函数
-def xmltojson():
-    xml_file = open('/Volumes/work/pythonSpace/ShowMoney/data/A_Verification.xml', encoding='utf-8')
-    xml_str = xml_file.read()
+xml_file = open('/Volumes/work/pythonSpace/ShowMoney/data/A_Verification.xml', encoding='utf-8')
+xml_str = xml_file.read()
 
-    # parse是的xml解析器e = xmlDom.parse(xmlstr)
-    xmlParse = xmltodict.parse(xml_str)
+# parse是的xml解析器e = xmlDom.parse(xmlstr)
+xmlParse = xmltodict.parse(xml_str)
 
-    # json库dumps()是将dict转化成json格式，loads()是将json转化成dict格式。
-    # dumps()方法的ident=1，格式化json
-    jsonStr = json.dumps(xmlParse).replace('@', '')
+# json库dumps()是将dict转化成json格式，loads()是将json转化成dict格式。
+# dumps()方法的ident=1，格式化json
+jsonStr = json.dumps(xmlParse).replace('@', '')
 
-    # 解析响应
-    rs = jsonStr.text
-    print(rs)  # 输出响应的文本
+myClient = pymongo.MongoClient('mongodb://localhost:27017/')
+DB_List = myClient.list_database_names()
 
-    myClient = pymongo.MongoClient("mongodb://localhost:27017/")
-    myDB = myClient["showMoney"]
-    myCol = myDB["apiRequests"]
+insertTime = {"$set": {"insertTime": localTime}}
 
-    x = myCol.insert_one(jsonStr.json())
+if "showMoney" in DB_List:
+    print("---------- 数据库已存在！----------")
+    myDB = myClient['showMoney']
+    colList = myDB.list_collection_names()
+    if "codeInfo" in colList:  # 判断 sites 集合是否存在
+        print("---------- 集合已存在！----------")
 
-    print(x)
+        myCol = myDB["codeInfo"]
+        i = myCol.insert_one(json.loads(jsonStr))
+        myCol.update_one(json.loads(jsonStr), insertTime)
+        print(i.modified_count, "文档已修改")
 
-if __name__ == "__main__":
-    xmltojson()  # 调用转换函数
+        for x in myCol.find():
+            print(x)
+            p1 = {}  # 空字典
+            if x == p1:
+                print(1)
+            else:
+                print(2)
+
+    else:
+        print('---------- 集合不存在！----------')
+else:
+    print('---------- 数据库不存在！----------')
