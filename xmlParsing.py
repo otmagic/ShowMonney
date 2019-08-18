2d00b7daa6b9b6fe9bc6ffb9fb9b1ecbd40d343c
OTMAGIC <ot.shen@otmagic.com>
2019-08-18 23:01:37 +0800

    no message

--- xmlParsing.py
+++ xmlParsing.py
@@ -16,7 +16,7 @@ import time
 localTime = time.strftime("%Y%m%d%H%M%S")
 
 # 定义xml转json的函数
-xml_file = open('/Volumes/work/pythonSpace/ShowMoney/data/A_Verification.xml', encoding='utf-8')
+xml_file = open('./data/A_Verification.xml', encoding='utf-8')
 xml_str = xml_file.read()
 
 # parse是的xml解析器e = xmlDom.parse(xmlstr)
@@ -26,7 +26,8 @@ xmlParse = xmltodict.parse(xml_str)
 # dumps()方法的ident=1，格式化json
 jsonStr = json.dumps(xmlParse).replace('@', '')
 
-myClient = pymongo.MongoClient('mongodb://localhost:27017/')
+# myClient = pymongo.MongoClient('mongodb://localhost:27017/')
+myClient = pymongo.MongoClient('mongodb://10.1.1.224:27017/')
 DB_List = myClient.list_database_names()
 
 insertTime = {"$set": {"insertTime": localTime}}
@@ -35,9 +36,25 @@ if "showMoney" in DB_List:
     print("---------- 数据库已存在！----------")
     myDB = myClient['showMoney']
     colList = myDB.list_collection_names()
-    if "codeInfo" in colList:  # 判断 sites 集合是否存在
+    if "codeInfo" in colList:  # 判断 表是否存在 集合是否存在
         print("---------- 集合已存在！----------")
 
+        myCol = myDB["codeInfo"]
+        i = myCol.insert_one(json.loads(jsonStr))
+        myCol.update_one(json.loads(jsonStr), insertTime)
+        print("文档已修改")
+
+        for x in myCol.find():
+            print(x)
+            p1 = {}  # 空字典
+            if x == p1:
+                print(1)
+            else:
+                print(2)
+    else:
+        print('---------- 集合不存在！----------')
+        print('---------- 集合创建中 ··· ----------')
+        myCol = myDB["codeInfo"]
         myCol = myDB["codeInfo"]
         i = myCol.insert_one(json.loads(jsonStr))
         myCol.update_one(json.loads(jsonStr), insertTime)
@@ -50,8 +67,6 @@ if "showMoney" in DB_List:
                 print(1)
             else:
                 print(2)
-
-    else:
-        print('---------- 集合不存在！----------')
+        print('---------- 集合创建成功! ----------')
 else:
     print('---------- 数据库不存在！----------')
